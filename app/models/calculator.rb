1ee06c879550ebeff85869416eda785d5d5a24d4
bryantt23 <bryantt23@gmail.com>
2015-08-04 23:28:44 -0700

    fin

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -1,6 +1,6 @@
 class Calculator < ActiveRecord::Base
   validates :age, :initial_contribution, :yearly_contribution, :annual_interest_rate, presence: true,  :numericality => { :greater_than_or_equal_to => 0 }
-  
+
   def how_long_until_retirement(age)
     65-age
   end
